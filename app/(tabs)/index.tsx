3e7d730ceb1b89fe46c7fa3b3c59cf8bac6a99a5
Evan11e <evan.1ee@outlook.com>
2025-07-07 14:06:22 +0800

    add gluestack ui

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,3 +1,4 @@
+import { Button, ButtonText } from "@/components/ui/button";
 import { Text, View } from "react-native";
 
 
@@ -5,6 +6,9 @@ export default function HomeScreen() {
   return (
    <View className='flex-1 justify-center items-center bg-white'>
     <Text>Hello World</Text>
+    <Button size="md" variant="solid" action="primary">
+      <ButtonText>Hello World!</ButtonText>
+    </Button>
    </View>
   );
 }
